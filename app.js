d9fc5767ace5f7dab893ebf4bdc1a85b3da4bd98
Geethanjali S <135814021+geethanjaliiii@users.noreply.github.com>
2024-08-29 18:02:44 +0530

    Add files via upload
    React is the library for web and native user interfaces. Build user interfaces out of individual pieces called components written in JavaScript.

---
+++ app.js
@@ -0,0 +1,9 @@
+//------------------------basic--------------------------
+
+const heading = React.createElement("h1",{},"DOM manipulation by React")
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+//Passing the react element
+root.render(heading)
+
